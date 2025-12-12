73d162097c2c31f6cbbc93102c18434248c1097f
woidptr <62186908+woidptr@users.noreply.github.com>
2025-12-12 22:58:27 +0100

    Fix

--- src/client/modules/movement/ToggleSprint.cpp
+++ src/client/modules/movement/ToggleSprint.cpp
@@ -3,6 +3,20 @@
 #include <sdk/GameContext.hpp>
 #include <core/Signatures.hpp>
 
+ToggleSprint::ToggleSprint(SetupAndRenderHook* setupAndRenderHook) : Module("Toggle Sprint") {
+	setupAndRenderHook->registerCallback(
+		[&](CallbackContext& cbCtx, void* screenView, MinecraftUIRenderContext* renderCtx) {
+			void** vtable = *(void***)renderCtx;
+			uintptr_t staticVtable = reinterpret_cast<uintptr_t>(vtable) - reinterpret_cast<uintptr_t>(GetModuleHandle(NULL));
+			Logger::info(std::format("MinecraftUIRenderContext vtable address: 0x{:X}", staticVtable));
+			// Logger::info(std::format("Text alpha: {}", renderCtx->getTextAlpha()));
+			RectangleArea rect = RectangleArea{ 10, 300, 10, 300 };
+			mce::Color color = mce::Color{ 1.f, 1.f, 1.f, 1.f };
+			renderCtx->drawRectangle(rect, color, 1.f, 5);
+		}
+	);
+}
+
 void ToggleSprint::tickCallback(void* a1) {
 
 }
--- src/client/modules/movement/ToggleSprint.hpp
+++ src/client/modules/movement/ToggleSprint.hpp
@@ -6,19 +6,7 @@
 
 class ToggleSprint : public Module {
 public:
-	ToggleSprint(SetupAndRenderHook* setupAndRenderHook) : Module("Toggle Sprint") {
-		setupAndRenderHook->registerCallback(
-			[&](CallbackContext& cbCtx, void* screenView, MinecraftUIRenderContext* renderCtx) {
-				void** vtable = *(void***)renderCtx;
-				uintptr_t staticVtable = reinterpret_cast<uintptr_t>(vtable) - reinterpret_cast<uintptr_t>(GetModuleHandle(NULL));
-				Logger::info(std::format("MinecraftUIRenderContext vtable address: 0x{:X}", staticVtable));
-				// Logger::info(std::format("Text alpha: {}", renderCtx->getTextAlpha()));
-				RectangleArea rect = RectangleArea{ 10, 300, 10, 300 };
-				mce::Color color = mce::Color{ 1.f, 1.f, 1.f, 1.f };
-				renderCtx->drawRectangle(rect, color, 1.f, 5);
-			}
-		);
-	}
+	ToggleSprint(SetupAndRenderHook* setupAndRenderHook);
 
 	void tickCallback(void* a1);
 };
