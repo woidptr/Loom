9e26835e826da8a6e3ea17d89be031a9d7cec3d6
woidptr <62186908+woidptr@users.noreply.github.com>
2026-02-23 12:32:18 +0100

    Forgot to push assets

--- src/core/Resource.cpp
+++ src/core/Resource.cpp
@@ -13,12 +13,6 @@ constexpr uint8_t asset_2[] = {
     #embed "C:/Users/firel/source/repos/Loom/assets/icons/close.embed.png"
 };
 constexpr uint8_t asset_3[] = {
-    #embed "C:/Users/firel/source/repos/Loom/assets/test.embed.png"
-};
-constexpr uint8_t asset_4[] = {
-    #embed "C:/Users/firel/source/repos/Loom/assets/test2.embed.png"
-};
-constexpr uint8_t asset_5[] = {
     #embed "C:/Users/firel/source/repos/Loom/signatures.embed.json"
 };
 
@@ -28,13 +22,11 @@ struct EmbeddedResource {
     size_t size;
 };
 
-constexpr std::array<EmbeddedResource, 6> resources = {{
+constexpr std::array<EmbeddedResource, 4> resources = {{
     {"assets/fonts/Arimo_Medium.ttf", asset_0, sizeof(asset_0)},
     {"assets/fonts/Montserrat_Medium.ttf", asset_1, sizeof(asset_1)},
     {"assets/icons/close.png", asset_2, sizeof(asset_2)},
-    {"assets/test.png", asset_3, sizeof(asset_3)},
-    {"assets/test2.png", asset_4, sizeof(asset_4)},
-    {"signatures.json", asset_5, sizeof(asset_5)},
+    {"signatures.json", asset_3, sizeof(asset_3)},
 }};
 }
 
