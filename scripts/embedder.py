dd500513d5f95fc005245c9a26a3d07002a3bf50
woidptr <62186908+woidptr@users.noreply.github.com>
2026-02-23 13:33:01 +0100

    Fix

--- scripts/embedder.py
+++ scripts/embedder.py
@@ -50,8 +50,8 @@ def main() -> None:
     cpp_content.append("namespace {\n")
 
     for i, (rel_path, full_path) in enumerate(files_to_embed):
-        cpp_content.append(f"constexpr unsigned char asset_{i}[] = {{")
-        cpp_content.append(f'    static_cast<unsigned char>(#embed "{full_path}")')
+        cpp_content.append(f"constexpr uint8_t asset_{i}[] = {{")
+        cpp_content.append(f'    #embed "{full_path}"')
         cpp_content.append("};")
     
     cpp_content.append("")
